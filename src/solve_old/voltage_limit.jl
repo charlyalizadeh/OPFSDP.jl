0e10abf56f5b5c124f04bf0f48caeaa1ac3541e6
Charly Alizadeh <alizadehcharly@gmail.com>
2024-05-21 12:38:22 +0200

    feat: remove unused code

--- src/solve_old/voltage_limit.jl
+++
@@ -1,6 +0,0 @@
-function _define_voltage_limit_constraints!(model, network, vvm)
-    for (id, bus) in network.buses
-		@constraint(model, vvm["real(v$(id))*real(v$(id))"] + vvm["imag(v$(id))*imag(v$(id))"] <= bus.vmax^2)
-		@constraint(model, vvm["real(v$(id))*real(v$(id))"] + vvm["imag(v$(id))*imag(v$(id))"] >= bus.vmin^2)
-	end
-end
