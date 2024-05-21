441f6f55b1a5cdfd8b4a1d8b51007c3311ce3b43
Charly Alizadeh <alizadehcharly@gmail.com>
2024-05-21 12:40:21 +0200

    feat: move core utilities from OPFSDP.jl to core.jl

--- src/OPFSDP.jl
+++ src/OPFSDP.jl
@@ -1,87 +1,15 @@
 module OPFSDP
 
 using JuMP
+#using Ipopt
 using Mosek
 using MosekTools
 using SparseArrays
 using LinearAlgebra
 using Logging
 using Printf
-
-
-
-@enum CostType begin
-    piecewise_linear = 1
-    quadratic = 2
-end
-
-mutable struct Bus
-	id::Int
-	v::Complex
-	load::Complex
-	vmin::Float64
-	vmax::Float64
-
-    function Bus(id::Int=-1, v::Complex=complex(0., 0.), load::Complex=complex(0., 0.), vmin::Float64=0., vmax::Float64=0.)
-		new(id, v, load, vmin, vmax)
-    end
-end
-
-mutable struct Generator
-	id::Int
-    genid::Int
-	power::Complex
-	Pmin::Float64
-	Pmax::Float64
-	Qmin::Float64
-	Qmax::Float64
-    active_cost_coeff::Vector{Float64}
-    reactive_cost_coeff::Vector{Float64}
-    active_cost_type::Union{CostType,Nothing}
-    reactive_cost_type::Union{CostType,Nothing}
-
-	function Generator(id::Int, genid::Int,
-                       power::Complex=complex(0., 0.),
-                       Pmin::Float64=0., Pmax::Float64=0.,
-                       Qmin::Float64=0., Qmax::Float64=0.,
-					   active_cost_coeff::Vector{Float64}=Float64[], reactive_cost_coeff::Vector{Float64}=Float64[],
-                       active_cost_type::Union{CostType,Nothing}=nothing, reactive_cost_type::Union{CostType,Nothing}=nothing)
-		new(id, genid, power, Pmin, Pmax, Qmin, Qmax, active_cost_coeff, reactive_cost_coeff, active_cost_type, reactive_cost_type)
-	end
-end
-
-mutable struct Branch
-	from::Int
-	to::Int
-	admittance::Complex
-	susceptance::Float64
-	tf_ratio::Float64
-	tf_ps_angle::Float64
-	rateA::Float64
-	rateB::Float64
-	rateC::Float64
-end
-Base.copy(b::Branch) = Branch(b.from, b.to, b.admittance, b.susceptance, b.tf_ratio, b.tf_ps_angle, b.rateA, b.rateB, b.rateC)
-
-mutable struct PowerFlowNetwork
-	name::String
-	buses::Dict{Int,Bus}
-    buses_order::Vector{Int}
-	branches::Vector{Branch}
-    generators::Dict{Int,Vector{Generator}}
-    generators_order::Vector{Int}
-    ngen::Int
-
-    function PowerFlowNetwork(name::String="",
-                              buses::Dict{Int,Bus}=Dict{Int,Bus}(),
-                              buses_order::Vector{Int}=Int[],
-                              branches::Vector{Branch}=Branch[],
-                              generators::Dict{Int,Vector{Generator}}=Dict{Int,Vector{Generator}}(),
-                              generators_order::Vector{Int}=Int[],
-                              ngen::Int=0)
-        new(name, buses, buses_order, branches, generators, generators_order, ngen)
-    end
-end
+using JSON
+using DataStructures
 
 
 include("core.jl")
@@ -95,18 +23,38 @@ include("decompose/cliquetree.jl")
 include("decompose/chordal.jl")
 include("decompose/extension/cholesky.jl")
 
-include("solve/utils.jl")
-include("solve/branch_flow_limit.jl")
-include("solve/linking_constraint.jl")
-include("solve/objective.jl")
-include("solve/power_balance.jl")
-include("solve/power_limit.jl")
-include("solve/solve.jl")
-include("solve/utils.jl")
-include("solve/variables.jl")
-include("solve/voltage_limit.jl")
-include("solve/check.jl")
-include("solve/solve.jl")
+using .ReadMatpower
+
+
+#include("solve/solve_W_old/constants.jl")
+#include("solve/solve_W_old/variables.jl")
+#include("solve/solve_W_old/objective.jl")
+#include("solve/solve_W_old/utils.jl")
+#include("solve/solve_W_old/power_balance.jl")
+#include("solve/solve_W_old/power_constraints.jl")
+#include("solve/solve_W_old/voltage_constraints.jl")
+#include("solve/solve_W_old/linking_constraints.jl")
+#include("solve/solve_W_old/save.jl")
+#include("solve/solve_W_old/solve.jl")
+
+
+include("solve/solve_W/constants.jl")
+include("solve/solve_W/variables.jl")
+include("solve/solve_W/objective.jl")
+include("solve/solve_W/utils.jl")
+include("solve/solve_W/power_balance.jl")
+include("solve/solve_W/power_balance_dict.jl")
+include("solve/solve_W/power_constraints_dict.jl")
+include("solve/solve_W/power_constraints.jl")
+include("solve/solve_W/voltage_constraints.jl")
+include("solve/solve_W/voltage_constraints_dict.jl")
+include("solve/solve_W/linking_constraints.jl")
+include("solve/solve_W/save/save_power_balance.jl")
+include("solve/solve_W/solve.jl")
+
+include("solve/solve_X/solve.jl")
+
+include("io/save_W_model.jl")
 
 export read_matpower
 export compute_current_origin, compute_current_destination
--- src/core.jl
+++ src/core.jl
@@ -1,37 +1,125 @@
-function inv_branch_ft(branch)
-    branch_copy = copy(branch)
-    temp = branch_copy.from
-    branch_copy.from = branch_copy.to
-    branch_copy.to = temp
-    return branch_copy
+@enum CostType begin
+    piecewise_linear = 1
+    quadratic = 2
 end
 
-function get_branches_in(network, bus_id)
-    branches = []
-    for branch in network.branches
-        if branch.from == bus_id
-            push!(branches, inv_branch_ft(branch))
-        end
-        if branch.to == bus_id
-            push!(branches, branch)
-        end
+@enum BusType begin
+    PQ = 1
+    PV = 2
+    ref = 3
+    isolated = 4
+end
+
+mutable struct Bus
+	id::Int
+    type::BusType
+	v::Complex
+    load::Vector{ComplexF64}
+	vmin::Float64
+	vmax::Float64
+    shunt_admittance::Complex
+    baseKV::Float64
+
+    function Bus(id::Int=-1, type::BusType=1, v::Complex=complex(0., 0.), load::Vector{ComplexF64}=[], vmin::Float64=0., vmax::Float64=0., shunt_admittance::Complex=complex(0., 0.), baseKV::Float64=0)
+		new(id, type, v, load, vmin, vmax, shunt_admittance, baseKV)
     end
-    return branches
 end
 
-function get_branches_out(network, bus_id)
-    branches = []
-    for branch in network.branches
-        if branch.to == bus_id
-            push!(branches, inv_branch_ft(branch))
-        end
-        if branch.from == bus_id
-            push!(branches, branch)
-        end
+mutable struct Generator
+	busid::Int
+    genid::Int
+    genorder::Int
+	power::Complex
+	Pmin::Float64
+	Pmax::Float64
+	Qmin::Float64
+	Qmax::Float64
+    active_cost_coeff::Vector{Float64}
+    reactive_cost_coeff::Vector{Float64}
+    active_cost_type::Union{CostType,Nothing}
+    reactive_cost_type::Union{CostType,Nothing}
+
+	function Generator(id::Int, genid::Int, genorder::Int,
+                       power::Complex=complex(0., 0.),
+                       Pmin::Float64=0., Pmax::Float64=0.,
+                       Qmin::Float64=0., Qmax::Float64=0.,
+					   active_cost_coeff::Vector{Float64}=Float64[], reactive_cost_coeff::Vector{Float64}=Float64[],
+                       active_cost_type::Union{CostType,Nothing}=nothing, reactive_cost_type::Union{CostType,Nothing}=nothing)
+		new(id, genid, genorder, power, Pmin, Pmax, Qmin, Qmax, active_cost_coeff, reactive_cost_coeff, active_cost_type, reactive_cost_type)
+	end
+end
+
+mutable struct Branch
+	from::Int
+	to::Int
+    r::Float64
+    x::Float64
+    b::Float64
+	tf_ratio::Float64
+	tf_ps_angle::Float64
+	rateA::Float64
+	rateB::Float64
+	rateC::Float64
+    angmin::Float64
+    angmax::Float64
+end
+Base.copy(b::Branch) = Branch(b.from, b.to, b.r, b.x, b.b, b.tf_ratio, b.tf_ps_angle, b.rateA, b.rateB, b.rateC, b.angmin, b.angmax)
+
+mutable struct PowerFlowNetwork
+	name::String
+	buses::Dict{Int,Bus}
+    buses_order::Vector{Int}
+	branches::Vector{Branch}
+    generators::Dict{Int,Vector{Generator}}
+    generators_order::Vector{Int}
+    ngen::Int
+
+    function PowerFlowNetwork(name::String="",
+                              buses::Dict{Int,Bus}=Dict{Int,Bus}(),
+                              buses_order::Vector{Int}=Int[],
+                              branches::Vector{Branch}=Branch[],
+                              generators::Dict{Int,Vector{Generator}}=Dict{Int,Vector{Generator}}(),
+                              generators_order::Vector{Int}=Int[],
+                              ngen::Int=0)
+        new(name, buses, buses_order, branches, generators, generators_order, ngen)
     end
-    return branches
 end
 
-function generators(network::PowerFlowNetwork)
-    return vcat([gen for gen in values(network.generators)]...)
+generators(network::PowerFlowNetwork) = vcat([gen for gen in values(network.generators)]...)
+function generators(network::PowerFlowNetwork, bus_id)
+    if bus_id in keys(network.generators)
+        return network.generators[bus_id]
+    end
+    return []
+end
+buses(network::PowerFlowNetwork) = collect(values(network.buses))
+branches(network::PowerFlowNetwork) = network.branches
+branches(network::PowerFlowNetwork, bus_id) = [b for b in network.branches if (b.from == bus_id || b.to == bus_id)]
+branches(network::PowerFlowNetwork, bus_id1, bus_id2) = [b for b in network.branches if (b.from == bus_id1 && b.to == bus_id2) || (b.from == bus_id2 && b.to == bus_id1)]
+nbus(network::PowerFlowNetwork) = length(network.buses)
+ngen(network::PowerFlowNetwork) = network.ngen
+nbranch(network::PowerFlowNetwork) = length(network.branches)
+normid(network::PowerFlowNetwork, bus_id) = findfirst(id -> id == bus_id, network.buses_order)
+trueid(network::PowerFlowNetwork, norm_id) = network.buses_order[norm_id]
+function refbus(network::PowerFlowNetwork)
+    refbuses = filter(b -> b.type == ref, collect(values(network.buses)))
+    return refbuses[1]
+end
+
+
+function hasgen(network, bus_id)
+    return bus_id in keys(network.generators)
+end
+
+function hasgencost(network)
+    return any([!isempty(gen.active_cost_coeff) for gen in generators(network)])
+end
+
+function hasbranch_directed(network, from, to)
+    return !isnothing(findfirst(b -> b.from == from && b.to == to, network.branches))
+end
+
+function hasbranch(network, b, a)
+    return !isnothing(findifrst(b -> (b.from == b && b.to == a) || (b.to == b && b.from == a),
+                                network.branches))
 end
